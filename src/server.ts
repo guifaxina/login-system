b969569765a84883acc2de4e00d71eb992aef4f7
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-04-18 20:33:42 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import router from "./routes/routes";
+import cors from "cors";
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN_URL
+}))
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use("/api/v1", router)
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/v1", router);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

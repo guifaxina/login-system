a41a21fc9b8be8557ce010b99d161679b42be3db
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-04-19 09:19:12 -0300

    feat: add user auth middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import authController from "../controllers/auth-controller";
 import emailController from "../controllers/email-controller";
 import userController from "../controllers/user-controller";
 import validationController from "../controllers/validation-controller";
@@ -7,10 +8,8 @@ const router = Router();
 
 router.post("/create-account", validationController.validate, userController.createAccount, emailController.sendConfirmationEmail);
 
-router.post("/login");
+router.post("/login", authController.login);
 
 router.get("/verify/:activationCode", emailController.verifyEmailAddress);
-
-
 
 export default router;
